21d2b6400f458393a160dbff4e079665a7382228
Jean <83733853+Jean-Delfino@users.noreply.github.com>
2023-12-27 17:06:30 -0300

    Visual Component for option menu code simplification.

---
+++ Assets/Scripts/UI/UISettingVisualComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI
+{
+    [Serializable]
+    public class UISettingVisualComponent
+    {
+        [SerializeField] private GameObject on;
+        [SerializeField] private GameObject off;
+
+        public void SetComponent(float value)
+        {
+            bool state = value == 0f;
+            
+            on.SetActive(!state);
+            off.SetActive(state);
+        }
+    }
+}
