4f655600d4aaa2c46f724fe9446b965367d406df
Jean <83733853+Jean-Delfino@users.noreply.github.com>
2023-12-31 16:01:22 -0300

    Find Language script for auto find in first time playing.

---
+++ Assets/Scripts/FindLanguage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using Reuse.CSV;
+using Reuse.Utils;
+using SaveGame;
+using UnityEngine;
+
+public class FindLanguage : MonoBehaviour
+{
+    [SerializeField] private string languageKey = "id_language_code";
+    private void Start()
+    {
+        if (UtilCardSave.LoadLanguage() > -1) return;
+        
+        StartCoroutine(WaitAndSetLanguage());
+    }
+
+    private IEnumerator WaitAndSetLanguage()
+    {
+        yield return new WaitUntil(GameVersatileTextsLocator.HasBeenInitialized);
+        var language = UtilGameVersatileText.FindComputerLanguage(languageKey);
+        if(language == -1) yield break;
+        
+        GameVersatileTextsController.ChangeActualLanguage(language);
+        yield return null;
+    }
+}
